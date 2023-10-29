a534e778252f7336cfc8ca4e1c587413f11d08c4
magon-miso <m.agonf@uniandes.edu.co>
2023-10-29 11:21:03 -0500

    add model to projects-ms

---
+++ projects/model.py
@@ -0,0 +1,72 @@
+from enum import Enum
+from email.policy import default
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields, Schema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+db = SQLAlchemy()
+
+class Project(db.Model):
+    __tablename__ = "project"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    type = db.Column(db.String(50))
+    leader = db.Column(db.String(50))
+    role = db.Column(db.String(20))
+    phone = db.Column(db.String(20))
+    email = db.Column(db.String(50))
+    countryId: db.Column(db.String(2))
+    cityId db.Column(db.Integer)
+    address db.Column(db.String(50))
+    companyId = db.Column(db.Integer)
+    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    profiles = db.relationship('Profile', cascade='all, delete, delete-orphan')
+
+    # def __repr__(self):
+    #     return f'<Project "{self.name}">'
+
+@property
+def createdAt(self):
+    return self.createdAt.isoformat()
+
+class Profile(db.Model):
+    __tablename__ = "profile"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    professional = db.Column(db.String(50))
+    projectId = db.Column(db.Integer, db.ForeignKey('project.id'))
+    softskills = db.relationship('SkillProfile', cascade='all, delete, delete-orphan')
+    techskills = db.relationship('SkillProfile', cascade='all, delete, delete-orphan')
+
+class SkillProfile(db.Model):
+    __tablename__ = "skill_profile"
+    id = db.Column(db.Integer, primary_key=True)
+    skillId = db.Column(db.Integer) # id skill microservice 
+    profileId = db.Column(db.Integer, db.ForeignKey('profile.id'))
+
+# queries: 
+# customer = session.query(Customer).get(1)
+# orders = customer.orders
+
+class ProjectSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Project
+        include_relationships = True
+        #include_fk = True
+        load_instance = True
+    profiles = fields.List(fields.Nested(ProfileSchema()))
+
+class ProfileSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Profile
+        include_relationships = True
+        include_fk = True
+        load_instance = True
+
+class SkillProfileSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = SkillProfile
+        include_relationships = True
+        include_fk = True
+        load_instance = True
---
+++ projects/view.py
@@ -0,0 +1,40 @@
+import json, requests
+from flask import request, current_app
+from flask_restful import Resource
+from model import db, Prueba, PruebaSchema
+from datetime import datetime
+
+test_schema = PruebaSchema()
+
+class HealthCheck(Resource):
+    def get(self):
+        return "ok"
+
+class GetProject(Resource):
+
+    def get(self, id):
+        # resp = validate_token(request.headers)
+        # if(resp['status_code'] != 200):
+        #     return resp['msg'], resp['status_code']
+
+        if id is not None: 
+            try:
+                int(id)
+            except ValueError:
+                data = {'error': 'id {} is not a number'.format(id)}
+                return json.dumps(data), 400
+
+        test = Prueba.query.filter(Prueba.id == id).first()
+        if test is None:
+            data = {'error': 'prueba {} does not exist'.format(id)}
+            return json.dumps(data), 404
+
+        return test_schema.dump(test)
+
+
+class ViewProjects(Resource):
+
+    def get(self):
+        tests = db.session.query(Prueba).select_from(Prueba).all()
+        return [test_schema.dump(t) for t in tests]
+
