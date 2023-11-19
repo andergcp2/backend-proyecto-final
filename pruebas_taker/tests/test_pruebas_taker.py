b01b2511bbe3169b0812202c71c667e93eebfe6d
magon-miso <m.agonf@uniandes.edu.co>
2023-11-18 23:13:00 -0500

    pruebas-taker - pruebasInit unit-tests advance

--- pruebas_taker/tests/test_pruebas_taker.py
+++ pruebas_taker/tests/test_pruebas_taker.py
@@ -5,7 +5,7 @@ from faker import Faker
 from unittest import TestCase
 from unittest.mock import patch
 
-class TestPruebasOrquestador(TestCase):
+class TestPruebasTaker(TestCase):
 
     def setUp(self):
         self.client = app.test_client()
@@ -14,54 +14,119 @@ class TestPruebasOrquestador(TestCase):
         self.token = "mangocat"
         self.headers_token = {'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)}
         
-        # prueba = Prueba(name='test-'+ self.data_factory.job() +'-'+ self.data_factory.company(), categoryId=self.data_factory.random_number(digits=2, fix_len=True))
-        # db.session.add(prueba)
-        # db.session.commit()
-        # self.id_prueba = db.session.query(Prueba).filter(Prueba.name==prueba.name, Prueba.categoryId==prueba.categoryId).first().id
-        #print(self.id_prueba, "=>", prueba.name, prueba.categoryId, prueba.createdAt)
-
         self.endpoint_health = '/pruebas-taker/ping'
-        self.endpoint_init_400 = '/pruebas-taker/init/candidatoId/pruebaId'
-        self.endpoint_init_404 = '/pruebas-taker/init/3600/9000'
-        self.endpoint_init_200 = '/pruebas-taker/init/1/37'
-        self.endpoint_next_200 = '/pruebas-taker/init/1/37'
+        self.endpoint_init_400_candidatoId = '/pruebas-taker/init/id/1'
+        self.endpoint_init_400_pruebaId = '/pruebas-taker/init/1/id'
+        self.endpoint_init_404 = '/pruebas-taker/init/1/4'
+        self.endpoint_init_200 = '/pruebas-taker/init/1/4'
+        self.endpoint_next_200 = '/pruebas-taker/init/1/4'
         #self.endpoint_init_200 = '/pruebas/init/9/45'
         #self.endpoint_init_200 = '/pruebas-orquestador/{}/{}'.format(str(self.id_prueba), str(self.id_prueba))
+
+        # self.post_resp_ok = {'msg': {"id": self.postId, "routeId": self.routeId, "userId": self.userId}, 'status_code': 200}
+        # self.route_resp_ok = {'msg': {"id": self.routeId, "bagCost": self.bagCost}, 'status_code': 200}
+        # self.offer_resp_ok = {'msg': {"id": self.offerId, "userId": self.id, "createdAt": datetime.now().isoformat()}, 'status_code': 201}
+
+        self.candidato_200 = {
+            "softSkills": [{"skill": "Teamwork"}, {"skill": "Communication"}],
+            "technicalSkills": [{"skill": "Back-End Development"}, {"skill": "Cloud Computing"}],
+            "id": 1, 
+            "name": "Alexandra", 
+            "lastName": "Suarez", 
+            "username": "alexaSz1",
+            "createdAt": "2023-11-12T23:26:05.081973"            
+        }
+
+        self.prueba_200 = {
+            "profiles": [{"id": 8, "profile": "Forest/woodland manager"}, {"id": 9, "profile": "Curator"}],
+            "techSkills": [{"id": 8, "skill": "field"}, {"id": 9, "skill": "economic"}, {"id": 10, "skill": "gas"}],
+            "questions": [
+                {"id": 28, "question": "Build city capital.", "level": 2 },
+                {"id": 29, "question": "Expect wish.", "level": 1 },
+                {"id": 30, "question": "Manage something book.", "level": 2},
+                {"id": 31, "question": "Hour color home.", "level": 4},
+                {"id": 32, "question": "Science you.", "level": 1},
+                {"id": 33, "question": "Fear door chair.", "level": 1},
+                {"id": 34, "question": "Character future board.", "level": 4},
+                {"id": 35, "question": "Art.", "level": 3},
+                {"id": 36, "question": "Their energy environmental.","level": 5}
+            ],
+            "id": 4,
+            "name": "Electrical engineer implement rich e-markets",
+            "numQuestions": 6,
+            "minLevel": 2,
+            "createdAt": "2023-11-12T22:57:59.819296"
+        }
+
+        self.prueba_candidato_200 = {
+            "id": 6,
+            "idcandidate": 1,
+            "idtest": 4,
+            "maxdatepresent": "2023-11-28",
+            "testestatus": "ASIGNADA",
+            "createdAt": "2023-11-18T22:29:16.157050"
+        }        
 
     def test_health_check(self):
         req_health = self.client.get(self.endpoint_health, headers={'Content-Type': 'application/json'})
         self.assertEqual(req_health.status_code, 200)
 
-    @patch('view.getPost')
-    def test_create_offer_404_post_not_found(self, mock_post, mock_token):
-        mock_token.return_value = self.token_resp_ok
-        mock_post.return_value = {'msg': 'post not found by mango', 'status_code': 404}
-        req = self.client.post(self.endpoint_create, data=json.dumps(self.new_offer), headers=self.headers_token)
-        json.loads(req.get_data())
-        #print("test-404: ", resp)
+    def test_init_test_400_candidatoId(self):
+        req_get = self.client.post(self.endpoint_init_400_candidatoId, headers=self.headers_token)
+        self.assertEqual(req_get.status_code, 400)
+
+    def test_init_test_400_testId(self):
+        req_get = self.client.post(self.endpoint_init_400_pruebaId, headers=self.headers_token)
+        self.assertEqual(req_get.status_code, 400)
+
+    @patch('view.getCandidato')
+    def test_init_test_404_candidato_not_found(self, mock_candidato):
+        mock_candidato.return_value = {'msg': 'candidato was not found by mango', 'status_code': 404}
+        req = self.client.post(self.endpoint_init_404, headers=self.headers_token)
+        #print("test-404: ", json.loads(req.get_data())) 
         self.assertEqual(req.status_code, 404)
 
-    # def test_init_prueba_400(self):
-    #     req_get = self.client.post(self.endpoint_init_400, headers=self.headers_token)
-    #     print(req_get.get_data())
-    #     self.assertEqual(req_get.status_code, 400)
+    @patch('view.getCandidato')
+    @patch('view.getPrueba')
+    def test_init_test_404_test_not_found(self, mock_prueba, mock_candidato):
+        mock_candidato.return_value = {'msg': self.candidato_200, 'status_code': 200}
+        mock_prueba.return_value = {'msg': 'prueba was not found by mango', 'status_code': 404}
+        req = self.client.post(self.endpoint_init_404, headers=self.headers_token)
+        #print("test-404: ", json.loads(req.get_data()))
+        self.assertEqual(req.status_code, 404)
 
-    # @patch('view.getPost')    
-    # def test_create_412(self, mock_post, mock_token):
-    #     mock_token.return_value = self.token_resp_ok
-    #     mock_post.return_value = self.post_resp_same_user
-    #     req = self.client.post(self.endpoint_create, data=json.dumps(self.new_offer), headers=self.headers_token)
-    #     json.loads(req.get_data())
-    #     #print("test-412: ", resp)
-    #     self.assertEqual(req.status_code, 412)
+    @patch('view.getCandidato')
+    @patch('view.getPrueba')
+    @patch('view.getPruebaCandidato')    
+    def test_init_test_404_candidato_test_not_associated(self, mock_prueba_candidato, mock_prueba, mock_candidato):
+        mock_candidato.return_value = {'msg': self.candidato_200, 'status_code': 200}
+        mock_prueba.return_value = {'msg': self.prueba_200, 'status_code': 200}
+        mock_prueba_candidato.return_value = {'msg': 'prueba is not associated to candidato by mango', 'status_code': 404}
+        req = self.client.post(self.endpoint_init_404, headers=self.headers_token)
+        #print("test-404: ", json.loads(req.get_data()))
+        self.assertEqual(req.status_code, 404)
 
-    # def test_init_prueba_404(self):
+    @patch('view.getCandidato')
+    @patch('view.getPrueba')
+    @patch('view.getPruebaCandidato')
+    @patch('view.deleteCache')
+    def test_init_test_200(self, mock_delete_cache, mock_prueba_candidato, mock_prueba, mock_candidato):
+        mock_candidato.return_value = {'msg': self.candidato_200, 'status_code': 200}
+        mock_prueba.return_value = {'msg': self.prueba_200, 'status_code': 200}
+        mock_prueba_candidato.return_value = {'msg': self.prueba_candidato_200, 'status_code': 200}
+        mock_delete_cache.return_value = ''
+        req = self.client.post(self.endpoint_init_404, headers=self.headers_token)
+        # print("test-200: ", json.loads(req.get_data()))
+        self.assertEqual(req.status_code, 200)
+        
+
+    # def test_init_test_404(self):
     #     req_get = self.client.post(self.endpoint_init_404, headers=self.headers_token)
     #     print(req_get.get_data())
     #     self.assertEqual(req_get.status_code, 404)
 
     '''
-    def test_init_prueba_200(self):
+    def test_init_test_200(self):
         req_get = self.client.post(self.endpoint_init_200, headers=self.headers_token)
         resp_get = json.loads(req_get.get_data())
         print("")
--- pruebas_taker/view.py
+++ pruebas_taker/view.py
@@ -3,10 +3,10 @@ from flask import request, Response, current_app
 from flask_restful import Resource
 #from redis_client import RedisClient
 
-def getPrueba(endpoint, headers):
+def getCandidato(endpoint, headers):
     return get(endpoint, headers)
 
-def getCandidato(endpoint, headers):
+def getPrueba(endpoint, headers):
     return get(endpoint, headers)
 
 def getPruebaCandidato(endpoint, headers):
@@ -15,13 +15,15 @@ def getPruebaCandidato(endpoint, headers):
 def get(endpoint, headers):
     try:
         resp = requests.get(endpoint, headers = headers)
-        return resp        
-        # if (resp.status_code==200):
-        #     return {'msg': resp.json(), 'status_code': resp.status_code}
-        # return {'msg': resp.content, 'status_code': resp.status_code} 
+        if (resp.status_code==200):
+            #return json.dumps(resp.json()), resp.status_code
+            return {'msg': resp.json(), 'status_code': resp.status_code}
+        #return json.dumps(resp.content), resp.status_code
+        return {'msg': resp.content, 'status_code': resp.status_code} 
     except Exception as ex:
-        return {'msg': 'connection endpoint failed {} -> {}'.format(endpoint, ex), 'status_code': 500}
         # return (resp.json, resp.status_code, resp.headers.items())
+        #return json.dumps('connection endpoint failed {} -> {}'.format(endpoint, ex)), resp.status_code
+        return {'msg': 'connection endpoint failed {} -> {}'.format(endpoint, ex), 'status_code': 500}
 
 def update(endpoint, data, headers):
     try:
@@ -30,6 +32,18 @@ def update(endpoint, data, headers):
     except Exception as ex:
         return {'msg': 'connection endpoint failed {} -> {}'.format(endpoint, ex), 'status_code': 500}
         #return (resp.json, resp.status_code, resp.headers.items())
+
+def deleteCache(key):
+    self.redis.delete(key)
+
+def setCache(key, data):
+    self.redis.hset(key, mapping=data)
+    #self.redis.rpush(key, json.dumps(data))
+
+def getCache(key):
+    return self.redis.hgetall(key)
+    #return json.loads(self.redis.lpop(key))
+
 
 class HealthCheck(Resource):
     def get(self):
@@ -43,91 +57,125 @@ class PruebaInit(Resource):
         super().__init__()
 
     def post(self, candidatoId, pruebaId):
+        testing = current_app.config['TESTING']
         headers = {"Content-Type":"application/json", "Authorization": request.headers['Authorization']}
+
+        if candidatoId is not None: 
+            try:
+                int(candidatoId)
+            except ValueError:
+                return "candidato id is not a number", 400
+
+        if pruebaId is not None: 
+            try:
+                int(pruebaId)
+            except ValueError:
+                return "prueba id is not a number", 400
 
         # 400 - En el caso que alguno de los campos no esté presente en la solicitud
         if candidatoId is None or pruebaId is None: 
             return "parameter(s) missing", 400
 
-        print(current_app.config['CANDIDATOS_QUERY'])
         endpoint = format(current_app.config['CANDIDATOS_QUERY']) +"/{}".format(candidatoId)
+        if not testing:
+            print ("candidato-url: ", endpoint)
         resp = getCandidato(endpoint, headers)
-        print ("candidato-resp: ", endpoint, resp.json())
+        #print ("candidato-resp: ", endpoint, resp)
+        if(resp['status_code'] != 200):
+            # 404 - El candidato que va a tomar la prueba no existe
+            return resp, resp['status_code'] # Response(resp['msg'], resp['status_code']) resp.headers.items()
+        candidato = resp['msg']
 
-        # 404 - El candidato que va iniciar la prueba no existe
-        if(resp.status_code != 200):
-            return Response(resp.json(), resp.status_code, resp.headers.items())
-
-        print(current_app.config['PRUEBAS_QUERY'])
         endpoint = format(current_app.config['PRUEBAS_QUERY']) +"/{}".format(pruebaId)
-        resp = get(endpoint, headers)
-        print ("prueba-resp: ", endpoint, resp.json())
+        if not testing:
+            print ("prueba-url: ", endpoint)
+        resp = getPrueba(endpoint, headers)
+        #print ("prueba-resp: ", endpoint, resp)
+        if(resp['status_code'] != 200):
+            # 404 - La prueba a iniciar no existe
+            return resp, resp['status_code']
+        prueba = resp['msg']
 
-        # 404 - La prueba a iniciar no existe
-        if(resp.status_code != 200):
-            return Response(resp.json(), resp.status_code, resp.headers.items())
-
-        print(current_app.config['CANDIDATOS_PRUEBAS'])
         endpoint = format(current_app.config['CANDIDATOS_PRUEBAS']) +"/{}/{}".format(candidatoId, pruebaId)
-        resp = get(endpoint, headers)
-        print ("candidatos-pruebas-res: ", endpoint, resp.json())
+        if not testing:
+            print ("candidato-prueba-url: ", endpoint)
+        resp = getPruebaCandidato(endpoint, headers)
+        #print ("candidato-prueba-res: ", endpoint, resp)
+        if(resp['status_code'] != 200):
+            # 404 - El candidato no está asociado a la prueba
+            return resp, resp['status_code']
 
-        # 404 - El candidato que va iniciar la prueba no existe
-        if(resp.status_code != 200):
-            return Response(resp.json(), resp.status_code, resp.headers.items())
+        idcache = pruebaId+"-"+candidatoId
+        deleteCache(idcache)
+
+        # for x in range(0, len(prueba['questions'])):
+        #     y=x
+        #     answers = []
+        #     question = questions[x]['question']
+        #     answers.append(questions[y]['answer'])
+        #     y+=1
+        #     answers.append(questions[y]['answer'])
+        #     y+=1
+        #     answers.append(questions[y]['answer'])
+        #     y+=1
+        #     answers.append(questions[y]['answer'])
+        #     y+=1
+        #     answers.append(questions[y]['answer'])
+
+        #     data = {'question': question, 'answers': answers}
+        #     pushCache(idcache, data)
 
         mango = True
         data = {
-            "pruebaId": pruebaId,
-            "candidatoId": candidatoId
+            "prueba": prueba,
+            "candidato": candidato,
+            "questions": prueba['questions'],
+            "totalQuestions": prueba['numQuestions']
         }
+
+        print()
+        print(data)
         if (mango):
             return json.dumps(data), 200
 
-        idcache = pruebaId+"-"+candidatoId
-        #self.redis.delete(idcache)
+        data = {'question': question, 'answers': answers}
+        setCache(idcache, data)
 
-        questionsTest = []
-        questions = json.loads(resp.json())
-        # se cargan 10 preguntas por default
-        for x in range(0, 50, 5):
-            y=x
-            answers = []
-            question = questions[x]['question']
-            #print("")
-            #print(x, question)
-            answers.append(questions[y]['answer'])
-            y+=1
-            answers.append(questions[y]['answer'])
-            y+=1
-            answers.append(questions[y]['answer'])
-            y+=1
-            answers.append(questions[y]['answer'])
-            y+=1
-            answers.append(questions[y]['answer'])
-            #print(x, y, answers)
-
-            data = {'question': question, 'answers': answers}
-            self.redis.rpush(idcache, json.dumps(data))
-
-        pregunta = json.loads(self.redis.lpop(idcache))
+        prueba = getCache(idcache)
+        
         data = {
             "pruebaId": pruebaId,
             "candidatoId": candidatoId,
+            "totalQuestions": prueba['numQuestions'], 
+            "numQuestion": 1, 
             "question": pregunta['question'],
-            "answers": pregunta['answers']
+            #"answers": pregunta['answers']
         }
         return json.dumps(data), 200
+
 
 class PruebaNext(Resource):
 
     def post(self, candidatoId, pruebaId):
+        if candidatoId is not None: 
+            try:
+                int(candidatoId)
+            except ValueError:
+                return "candidato id is not a number", 400
+
+        if pruebaId is not None: 
+            try:
+                int(pruebaId)
+            except ValueError:
+                return "prueba id is not a number", 400
+
         # 400 - En el caso que alguno de los campos no esté presente en la solicitud
         if candidatoId is None or pruebaId is None: 
             return "parameter(s) missing", 400
 
         idcache = pruebaId+"-"+candidatoId
-        pregunta = json.loads(self.redis.lpop(idcache))
+        pregunta = popCache(idcache)
+        #pregunta = json.loads(self.redis.lpop(idcache))
         data = {
             "pruebaId": pruebaId,
             "candidatoId": candidatoId,
