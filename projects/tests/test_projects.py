0d538dedfa039bc3833351df1b9e96d7ee187344
magon-miso <m.agonf@uniandes.edu.co>
2023-11-24 12:54:06 -0500

    projects - add associate candidate endpoint

--- projects/tests/test_projects.py
+++ projects/tests/test_projects.py
@@ -1,6 +1,6 @@
 import json
 from app import app
-from model import db, Project, Profile, SkillProfile, TestProfile
+from model import db, Project, Profile, SoftSkillProfile, TechSkillProfile, TestProfile
 from faker import Faker
 from unittest import TestCase
 from unittest.mock import patch
@@ -50,6 +50,16 @@ class TestProjects(TestCase):
             "profiles": profiles
         }
 
+        self.candidato = {
+            "id": self.fake.random_int(1000, 1999), 
+            "name": self.fake.first_name(), 
+            "lastName": self.fake.last_name(),
+            "username": self.fake.email(),
+            "softSkills": [{"skill": self.fake.word()}, {"skill": self.fake.word()}],
+            "technicalSkills": [{"skill": self.fake.job()}, {"skill": self.fake.job()}],
+            "createdAt": "2023-11-12T23:26:05.081973"
+        }
+
         self.endpoint_health = '/projects/ping'
         self.endpoint = '/projects'
         self.endpoint_get_412 = '/projects/id'
@@ -59,6 +69,11 @@ class TestProjects(TestCase):
         self.endpoint_get_tests_404 = '/projects/{}/tests'.format(str(self.fake.random_int(10, 99) * 10))
         self.endpoint_get_projects_company_200 = '/projects/companies/{}'.format(self.project["company"])
         self.endpoint_get_projects_company_200_empty = '/projects/companies/{}'.format(self.project["company"]*10)
+
+        self.endpoint_post_cand_proj_400_project = '/projects/projectId/candidates/candidatoId'
+        self.endpoint_post_cand_proj_400_candidate = '/projects/0/candidates/candidatoId'
+        self.endpoint_post_candidate_project_404 = '/projects/0/candidates/0'
+        self.endpoint_post_candidate_project_201 = '/projects/{}/candidates/{}'.format(0, 0)
 
     def test_health_check(self):
         req_health = self.client.get(self.endpoint_health, headers={'Content-Type': 'application/json'})
@@ -143,3 +158,64 @@ class TestProjects(TestCase):
         data = json.loads(req_get.get_data())
         self.assertEqual(req_get.status_code, 200)
         self.assertGreater(len(data), 0)
+
+    def test_candidate_project_400(self):
+        resp_create = self.client.post(self.endpoint_post_cand_proj_400_project, headers=self.headers_token)
+        data = json.loads(resp_create.get_data())
+        self.assertEqual(resp_create.status_code, 400)
+
+    def test_candidate_project_400_candidate(self):
+        resp_create = self.client.post(self.endpoint_post_cand_proj_400_candidate, headers=self.headers_token)
+        data = json.loads(resp_create.get_data())
+        self.assertEqual(resp_create.status_code, 400)
+
+    def test_candidate_project_404_project(self):
+        resp_create = self.client.post(self.endpoint, headers=self.headers_token, data=json.dumps(self.project))
+        data = json.loads(resp_create.get_data())
+        self.assertEqual(resp_create.status_code, 201)
+
+        self.endpoint_get_200 = '/projects/{}'.format(data["id"])
+        resp_get = self.client.get(self.endpoint_get_200, headers=self.headers_token)
+        data = json.loads(resp_get.get_data())
+        self.assertEqual(resp_get.status_code, 200)
+
+        self.endpoint_post_candidate_project_404 = '/projects/{}/candidates/0'.format(data["id"]*1000)
+        resp_create = self.client.post(self.endpoint_post_candidate_project_404, headers=self.headers_token)
+        data = json.loads(resp_create.get_data())
+        self.assertEqual(resp_create.status_code, 404)
+
+    @patch('view.getCandidato')
+    def test_candidate_project_404_candidate(self, mock_candidato):
+        resp_create = self.client.post(self.endpoint, headers=self.headers_token, data=json.dumps(self.project))
+        data = json.loads(resp_create.get_data())
+        self.assertEqual(resp_create.status_code, 201)
+
+        self.endpoint_get_200 = '/projects/{}'.format(data["id"])
+        resp_get = self.client.get(self.endpoint_get_200, headers=self.headers_token)
+        data = json.loads(resp_get.get_data())
+        self.assertEqual(resp_get.status_code, 200)
+
+        self.endpoint_post_candidate_project_404 = '/projects/{}/candidates/0'.format(data["id"])
+        mock_candidato.return_value = {'msg': 'candidato was not found by mango', 'status_code': 404}
+        resp_create = self.client.post(self.endpoint_post_candidate_project_404, headers=self.headers_token)
+        data = json.loads(resp_create.get_data())
+        self.assertEqual(resp_create.status_code, 404)        
+
+    @patch('view.getCandidato')
+    def test_candidate_project_201(self, mock_candidato):
+        resp_create = self.client.post(self.endpoint, headers=self.headers_token, data=json.dumps(self.project))
+        data = json.loads(resp_create.get_data())
+        self.assertEqual(resp_create.status_code, 201)
+
+        self.endpoint_get_200 = '/projects/{}'.format(data["id"])
+        resp_get = self.client.get(self.endpoint_get_200, headers=self.headers_token)
+        data = json.loads(resp_get.get_data())
+        self.assertEqual(resp_get.status_code, 200)
+
+        self.endpoint_post_candidate_project_404 = '/projects/{}/candidates/{}'.format(data["id"], self.candidato["id"])
+        mock_candidato.return_value = {'msg': self.candidato, 'status_code': 200}
+        resp_create = self.client.post(self.endpoint_post_candidate_project_404, headers=self.headers_token)
+        data = json.loads(resp_create.get_data())
+        # print()
+        # print(data)
+        self.assertEqual(resp_create.status_code, 201)
